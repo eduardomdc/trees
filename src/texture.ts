957b64cc349a96a0453ba808a2a79f61a7eb46d1
eduardomc <eduardomc@dcc.ufrj.br>
2026-05-05 11:32:51 -0300

    some textures

--- src/texture.ts
+++ src/texture.ts
@@ -7,4 +7,8 @@ export const LeafTextures = {
 }
 export const BarkTextures = {
     'CommonBark' : loader.load(import.meta.env.BASE_URL+'/assets/bark.jpg'),
+    'WhiteBark' : loader.load(import.meta.env.BASE_URL+'/assets/white_bark.jpg'),
+    'BlackBark' : loader.load(import.meta.env.BASE_URL+'/assets/black_bark.png'),
+    'SmoothBark' : loader.load(import.meta.env.BASE_URL+'/assets/smooth_bark.png'),
+    'YellowBark' : loader.load(import.meta.env.BASE_URL+'/assets/yellow_bark.png'),
 }
