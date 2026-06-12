e5cca1e3b06e3a83fa6a2b49e773336eb81c9464
eduardomdc <eduardomc@dcc.ufrj.br>
2026-06-12 15:54:20 -0300

    Async texture loading ^

--- src/texture.ts
+++ src/texture.ts
@@ -2,28 +2,28 @@ import * as THREE from 'three';
 
 const loader = new THREE.TextureLoader();
 export const LeafTextures = {
-    'CommonGreen' : loader.load(import.meta.env.BASE_URL+'/assets/greenleaf.png'),
-    'LongGreen' : loader.load(import.meta.env.BASE_URL+'/assets/longleaf.png'),
-    'MapleRed' : loader.load(import.meta.env.BASE_URL+'/assets/leaf.png'),
-    'PalmFrond' : loader.load(import.meta.env.BASE_URL+'/assets/palm_leaf.png'),
-    'PalmNeedle' : loader.load(import.meta.env.BASE_URL+'/assets/palm_needle.png'),
-    'Raffia' : loader.load(import.meta.env.BASE_URL+'/assets/raffia.png'),
+    'CommonGreen' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/greenleaf.png'),
+    'LongGreen' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/longleaf.png'),
+    'MapleRed' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/leaf.png'),
+    'PalmFrond' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/palm_leaf.png'),
+    'PalmNeedle' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/palm_needle.png'),
+    'Raffia' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/raffia.png'),
 }
 export const BarkTextures = {
-    'Common' : loader.load(import.meta.env.BASE_URL+'/assets/bark.jpg'),
-    'White' : loader.load(import.meta.env.BASE_URL+'/assets/white_bark.jpg'),
-    'Black' : loader.load(import.meta.env.BASE_URL+'/assets/black_bark.png'),
-    'Smooth' : loader.load(import.meta.env.BASE_URL+'/assets/smooth_bark.png'),
-    'Yellow' : loader.load(import.meta.env.BASE_URL+'/assets/yellow_bark.png'),
-    'Mossy1' : loader.load(import.meta.env.BASE_URL+'/assets/mossy1.jpg'),
-    'Mossy2' : loader.load(import.meta.env.BASE_URL+'/assets/mossy2.jpg'),
-    'Mossy3' : loader.load(import.meta.env.BASE_URL+'/assets/mossy3.jpg'),
-    'Mossy4' : loader.load(import.meta.env.BASE_URL+'/assets/mossy4.jpg'),
-    'WhiteMossy' : loader.load(import.meta.env.BASE_URL+'/assets/white_moss_bark.jpg'),
-    'New' : loader.load(import.meta.env.BASE_URL+'/assets/new_bark.png'),
-    'Old' : loader.load(import.meta.env.BASE_URL+'/assets/old_bark.jpg'),
-    'Gray' : loader.load(import.meta.env.BASE_URL+'/assets/gray_bark.jpg'),
-    'Grainy' : loader.load(import.meta.env.BASE_URL+'/assets/grainy_bark.jpg'),
+    'Common' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/bark.jpg'),
+    'White' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/white_bark.jpg'),
+    'Black' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/black_bark.png'),
+    'Smooth' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/smooth_bark.png'),
+    'Yellow' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/yellow_bark.png'),
+    'Mossy1' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/mossy1.jpg'),
+    'Mossy2' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/mossy2.jpg'),
+    'Mossy3' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/mossy3.jpg'),
+    'Mossy4' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/mossy4.jpg'),
+    'WhiteMossy' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/white_moss_bark.jpg'),
+    'New' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/new_bark.png'),
+    'Old' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/old_bark.jpg'),
+    'Gray' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/gray_bark.jpg'),
+    'Grainy' : await loader.loadAsync(import.meta.env.BASE_URL+'/assets/grainy_bark.jpg'),
 }
 
 Object.values(LeafTextures).forEach(tex => {
